80ed1c432e9e3b93fa694416848417c42857282b
username_anjastabuni <reblytabuni2004@gmail.com>
2023-07-17 20:38:53 +0700

    mengenal property filter

---
+++ app.js
@@ -0,0 +1,67 @@
+// Vue.filters('currencyFormat', function(){
+
+// });
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    maximum: 50,
+    products: null,
+    cart: [{ product: product, qty: qty }],
+    style: {
+      label: ["font-weight-bold", "mr-2"],
+      inputWidth: 60,
+      sliderStatus: true,
+    },
+  },
+  mounted: function () {
+    fetch("https://hplussport.com/api/products/order/price")
+      .then((Response) => Response.json())
+      .then((data) => {
+        this.products = data;
+      });
+  },
+  filters: {
+    currencyFormat: function (value) {
+      return "Rp" + Number.parseFloat(value).toFixed(2);
+    },
+  },
+  computed: {
+    sliderState: function () {
+      return this.style.sliderStatus ? "d-flex" : "d-none";
+    },
+  },
+  methods: {
+    before: function (el) {
+      el.className = "d-none";
+    },
+    enter: function (el) {
+      var delay = el.dataset.index * 100;
+      setTimeout(function () {
+        el.className = "row d-flex mb-3 align-items-center animated fadeInRight";
+      }, delay);
+    },
+    leave: function (el) {
+      var delay = el.dataset.index * 100;
+      setTimeout(function () {
+        el.className = "row d-flex mb-3 align-items-center animated fadeOutRight";
+      }, delay);
+    },
+    addItem: function (product) {
+      var productIndex;
+      var productExist = this.cart.filter(function (item, index) {
+        if (item.product.id == Number(product.id)) {
+          productIndex = index;
+          return true;
+        } else {
+          return false;
+        }
+      });
+      if (productExist.length) {
+        this.cart[productIndex].qty++;
+      } else {
+        this.cart.push({ product: product, qty: 1 });
+      }
+    },
+  },
+});
